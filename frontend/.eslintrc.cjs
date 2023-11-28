400ee772a4e1dab50f117201412f737e3648b6ec
yt20chill <ytelainechiu@gmail.com>
2023-11-28 09:01:11 +0800

    chore: react setup

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2020: true, node: true },
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/strict-type-checked",
+		"plugin:react-hooks/recommended",
+		"plugin:@typescript-eslint/stylistic-type-checked",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs", "tailwind.config.js"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["react-refresh"],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+	},
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		project: ["./tsconfig.json", "./tsconfig.node.json"],
+		tsconfigRootDir: __dirname,
+	},
+};
