9883b58f99c2027161695c674a64a23e523ef648
yt20chill <ytelainechiu@gmail.com>
2023-12-07 22:13:10 +0800

    added API routes for auth

--- frontend/src/features/api/authApi.ts
+++ frontend/src/features/api/authApi.ts
@@ -1,38 +1,49 @@
-import axios from "axios";
-import { FetchError, authApiRoutes } from "../../lib/apiUtils";
-import { SignInDto } from "../../types/authModels";
+import { FetchError, authApiRoutes, axiosWrapper } from "../../lib/apiUtils";
+import { AuthorizeLevel, SignInDto, SignUpDto } from "../../types/authModels";
 
-type ApiMethod = "get" | "post" | "put" | "delete";
-
-type BaseResult = {
-	success: boolean;
-};
-
-type SignInResult = BaseResult & {
-	token?: string;
+type BaseFetchResult = {
 	message?: string;
 };
 
-export const signInAJAX = async (signInDto: SignInDto) => {
-	const result = await axios.post<SignInResult>(
-		authApiRoutes.SIGN_IN,
-		signInDto
-	);
-	if (!result.data.success) {
-		throw new FetchError(result.data.message);
-	}
+type SignInResult = BaseFetchResult & {
+	jwt: string;
 };
 
-export const axiosWrapper = async <PayloadType, ResultType>(
-	method: ApiMethod,
-	url: string,
-	data: PayloadType
-): Promise<ResultType> => {
-	try {
-		const result = await axios<ResultType>({
-			method,
-			url,
-			data,
-		});
-	} catch (error) {}
+type SignUpResult = BaseFetchResult & {
+	jwt: string;
+};
+
+type GetAuthResult = BaseFetchResult & {
+	username: string;
+	role: string;
+};
+
+export const signInAJAX = async (signInDto: SignInDto) => {
+	const { jwt } = await axiosWrapper<SignInDto, SignInResult>(
+		authApiRoutes.SIGN_IN,
+		"post",
+		signInDto
+	);
+	if (jwt) return jwt;
+	throw new FetchError(500, "invalid token");
+};
+
+export const signUpAJAX = async (
+	signUpDto: Omit<SignUpDto, "confirmPassword">
+) => {
+	const { jwt } = await axiosWrapper<typeof signUpDto, SignUpResult>(
+		authApiRoutes.SIGN_UP,
+		"post",
+		signUpDto
+	);
+	if (jwt) return jwt;
+	throw new FetchError(500, "invalid token");
+};
+
+export const getAuthAJAX = async () => {
+	const { username, role } = await axiosWrapper<void, GetAuthResult>(
+		authApiRoutes.GET_AUTH
+	);
+	if (role === null) return { username, role: AuthorizeLevel.USER };
+	return { username, role: AuthorizeLevel.PROVIDER };
 };
--- frontend/src/lib/apiUtils.ts
+++ frontend/src/lib/apiUtils.ts
@@ -1,21 +1,49 @@
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 
 export const queryKey = Object.freeze({
 	REQUEST: "request",
 	OFFER: "offer",
 	ORDER: "order",
+	AUTH: "auth",
 });
 
 axios.defaults.baseURL = process.env.VITE_API_URL;
+axios.defaults.headers.common["Authorization"] =
+	`Bearer ${localStorage.getItem("token")}` ?? undefined;
+axios.defaults.headers.common["Content-Type"] = "application/json";
 
 export const authApiRoutes = Object.freeze({
 	SIGN_IN: `/api/user/login`,
 	SIGN_UP: `/api/user/registration`,
+	GET_AUTH: `/api/user`,
 });
 
 export class FetchError extends Error {
-	constructor(public readonly message: string = "Fetch Error") {
+	constructor(
+		public readonly status: number = 500,
+		public readonly message: string = "Fetch Error"
+	) {
 		super(message);
 		Object.setPrototypeOf(this, FetchError.prototype);
 	}
 }
+
+type ApiMethod = "get" | "post" | "put" | "delete";
+
+export const axiosWrapper = async <PayloadType, ResultType>(
+	url: string,
+	method: ApiMethod = "get",
+	data?: PayloadType
+): Promise<ResultType> => {
+	try {
+		const result = await axios<ResultType>({
+			method,
+			url,
+			data,
+		});
+		return result.data;
+	} catch (error) {
+		if (isAxiosError(error)) throw new FetchError(error.status, error.message);
+		throw new FetchError();
+	}
+};
