b1ff1b396ca188df1896ed112d7094372180de5b
yt20chill <ytelainechiu@gmail.com>
2023-12-07 22:11:28 +0800

    added useQuery

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
@@ -8,10 +9,15 @@ const rootElement = document.getElementById("root");
 if (!rootElement) {
 	throw new Error("Root element not found");
 }
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(rootElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
