3cecf71487b6c958b3a50c9964b95dc037067b20
CookedRicer <e.kuquer@gmail.com>
2023-12-08 15:27:09 +0800

    add Navbar

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,30 +1,45 @@
-import { authStore } from "@/features/stores/authStore";
+import { useAuthStore } from "@/features/stores/authStore";
+import routesConfig, { RouteEnum } from "@/pages/routes.config";
 import { Link } from "react-router-dom";
 
 export function Navbar() {
-  const userInfo = authStore<AuthState>((state) => state);
+  const userInfo = useAuthStore((state) => state);
   return (
-    <nav>
+    <nav className="flex justify-between p-5">
       {/* logo -> home page*/}
-      <Link to="/">
-        <div className=""></div>
-      </Link>
-      {/* nav_buttons */}
-      <Link to="/">
-        <div className=""></div>
-      </Link>
-      <Link to="/">
-        <div className=""></div>
-      </Link>
-      <Link to="/">
-        <div className=""></div>
-      </Link>
-      <Link to="/">
-        <div className=""></div>
-      </Link>
-      <Link to="/">
-        <div className=""></div>
-      </Link>
+      <div className="flex gap-5">
+        <Link to={routesConfig.get(RouteEnum.Home)?.path ?? "/"}>
+          <div className="Logo">LOGO</div>
+        </Link>
+        {/* nav_link_buttons */}
+        <Link to="/requests">
+          <div className="">Explore</div>
+        </Link>
+        <Link to="/user/request">
+          <div className="">Request</div>
+        </Link>
+        <Link to="/Order">
+          <div className="">Order</div>
+        </Link>
+      </div>
+      {/* nav_login_info */}
+      <div>
+        {userInfo.isAuthenticated ? (
+          <>
+            <Link to="/user/profile">
+              <div className="">Profile</div>
+            </Link>
+            <Link to="/user/profile">
+              <div className="">Profile</div>
+            </Link>
+          </>
+        ) : (
+          //Should be a dropdown list
+          <Link to="/sign-in">
+            <div className="">Sign-in</div>
+          </Link>
+        )}
+      </div>
     </nav>
   );
 }
