c1bb3988d93a1c5b4609bc24c944b91d3d01bd80
yt20chill <ytelainechiu@gmail.com>
2023-12-09 01:17:21 +0800

    added url to sign in and sign up

--- frontend/src/pages/public/SignInPage.tsx
+++ frontend/src/pages/public/SignInPage.tsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
 import SignInForm from "../../features/forms/SignInForm";
+import { RouteEnum, siteMap } from "../routes.config";
 
 function SignInPage() {
-	return <SignInForm />;
+	return (
+		<>
+			<SignInForm />
+			<p className="my-3">
+				New user? <Link to={siteMap(RouteEnum.SignUp)}>Sign up </Link>
+				for free!
+			</p>
+		</>
+	);
 }
 
 export default SignInPage;
