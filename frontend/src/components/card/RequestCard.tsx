2735edaa7ad226d97eebcd94c5795900bd1c6f14
CookedRicer <e.kuquer@gmail.com>
2023-12-11 11:31:17 +0800

    update dummy data and fin skeleton

--- frontend/src/components/card/RequestCard.tsx
+++ frontend/src/components/card/RequestCard.tsx
@@ -7,9 +7,9 @@ export function RequestCard(props: RequestDto) {
   return (
     <Link
       to={`/request-detail/${props.requestId}`}
-      className="p-2 rounded-lg shadow-sm border border-orange-200/25 hover:scale-[101%] transition-all bg-base-100/30 backdrop-blur-md"
+      className="icn-hvr-fill p-1 rounded-lg shadow-2xl hover:scale-105 transition-all bg-base-100/25 backdrop-blur-lg min-h-[340px] border border-base-200/75"
     >
-      <figure className="rounded-md h-48 flex justify-center bg-slate-300 overflow-hidden">
+      <figure className="rounded-md h-48 flex justify-center bg-slate-300 overflow-hidden border border-gray-200">
         <img
           className="object-cover w-full"
           title={props.item}
@@ -18,32 +18,32 @@ export function RequestCard(props: RequestDto) {
       </figure>
       <div className="px-2 pt-2">
         <div className="truncate">{props.item}</div>
-        <div className="flex items-center font-light text-sm gap-1 text-slate-500">
+        <div className="inline-flex items-center font-light text-sm gap-1 text-slate-500 ps-1 pe-2 border border-slate-500 rounded-badge">
           <span className="material-symbols-rounded text-sm">public</span>
           {props.locationName}
         </div>
 
         {props.offerPrice ? (
-          <div className="flex justify-end items-baseline font-bold text-2xl h-6 text-orange-800">
-            <span className="text-lg">$</span>
+          <div className="flex justify-end items-baseline text-2xl h-6 text-orange-800">
+            <span className="text-sm">Offer $</span>
             {props.offerPrice}
           </div>
         ) : (
-          <div className="flex justify-end items-center font-bold h-6 text-orange-500">
+          <div className="flex justify-end items-center h-6 text-secondary-500">
             <span className="material-symbols-rounded text-2xl">
               contact_support
             </span>
-            <div className="text-2xl">Offer</div>
+            <div className="text-lg">Please Offer</div>
           </div>
         )}
-        <div className="flex justify-end items-center gap-1">
+        <div className="flex justify-end items-center gap-1 ">
           <span className="material-symbols-rounded text-lg">
             account_circle
           </span>
           {props.createdBy}
         </div>
         <div
-          className="flex justify-end items-center gap-1 text-sm text-slate-400"
+          className="flex justify-end items-center gap-1 text-xs text-slate-400"
           title={props.updatedAt}
         >
           <span className="material-symbols-rounded text-lg">pace</span>
