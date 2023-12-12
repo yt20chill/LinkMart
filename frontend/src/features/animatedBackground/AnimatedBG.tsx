a4d203c0215df28b4c7613c0b6e23f3f6d0c947e
CookedRicer <e.kuquer@gmail.com>
2023-12-12 21:34:56 +0800

    Update AnimatedBG.tsx

--- frontend/src/features/animatedBackground/AnimatedBG.tsx
+++ frontend/src/features/animatedBackground/AnimatedBG.tsx
@@ -1,9 +1,10 @@
 export function AnimatedBG() {
   return (
     <div className="w-screen h-screen overflow-hidden fixed top-0 left-0 -z-50">
-      <div className="select-none rounded-full z-20 bg-animate absolute w-full h-full blur-3xl bg-amber-50/50 top-[-200px] left-0"></div>
-      <div className="select-none rounded-full z-10 bg-animate-reverse absolute w-1/2 h-1/2 blur-3xl bg-orange-500/25 bottom-[300px] right-[50%]"></div>
-      <div className="select-none rounded-full z-10 bg-animate absolute w-3/4 h-3/4 blur-3xl bg-amber-500/25 bottom-0 left-[50%]"></div>
+      <div className="select-none rounded-full z-30 absolute w-[80vw] h-[80vw] blur-3xl bg-amber-50/50 origin-center transform-center "></div>
+      <div className="select-none rounded-full z-10 bg-animate-reverse absolute w-96 h-96 blur-3xl bg-orange-500/25 bottom-1 right-1"></div>
+      <div className="select-none rounded-full z-10 absolute w-[calc(35vw)] h-[calc(35vw)] blur-xl bg-amber-300/25 bottom-1/2 right-3/4"></div>
+      <div className="select-none rounded-full z-20 absolute w-[calc(15vw)] h-[calc(15vw)] blur-3xl bg-amber-500/50 bottom-1/3 right-5/6"></div>
     </div>
   );
 }
