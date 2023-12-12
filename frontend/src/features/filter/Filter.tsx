40b523911b438b56f5adfa7cb0e3f8fd0b48493d
yt20chill <ytelainechiu@gmail.com>
2023-12-12 18:11:14 +0800

    style: changed loading animation

--- frontend/src/features/filter/Filter.tsx
+++ frontend/src/features/filter/Filter.tsx
@@ -31,24 +31,28 @@ export function Filter() {
 
 	return (
 		<>
-			{categories && locations ? (
-				<div className="max-w-full bg-gradient-to-r from-orange-300 to-amber-300/75 text-base-100 shadow">
-					<div className="max-w-7xl mx-auto">
+			<div className="max-w-full bg-gradient-to-r from-orange-300 to-amber-300/75 text-base-100 shadow">
+				<div className="max-w-7xl mx-auto">
+					{categories ? (
 						<FilterGroup
 							searchParamsWrapper={searchParamsWrapper}
 							name="Categories"
 							items={categories.map((category) => category.categoryName)}
 						/>
+					) : (
+						<span className="loading loading-dots loading-sm"></span>
+					)}
+					{locations ? (
 						<FilterGroup
 							searchParamsWrapper={searchParamsWrapper}
 							name="Countries"
 							items={locations.map((location) => location.locationName)}
 						/>
-					</div>
+					) : (
+						<span className="loading loading-dots loading-sm"></span>
+					)}
 				</div>
-			) : (
-				<div className="skeleton w-32 h-32"></div>
-			)}
+			</div>
 		</>
 	);
 }
