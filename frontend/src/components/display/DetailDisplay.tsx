0d2f4741c24ac59f6f5bdb191d7c4b99606f3666
CookedRicer <e.kuquer@gmail.com>
2023-12-14 14:10:28 +0800

    update components

---
+++ frontend/src/components/display/DetailDisplay.tsx
@@ -0,0 +1,25 @@
+type DetailDisplayProps = {
+  title?: string;
+  icon?: string;
+  label: string;
+  value: string;
+  className?: string;
+};
+
+export function DetailDisplay(props: DetailDisplayProps) {
+  return (
+    <div className={`mb-5 ${props.className}`}>
+      <div className="flex items-center font-roboto text-xs text-slate-400/80 tracking-wider leading-none h-4">
+        {props.icon && (
+          <span className="material-symbols-rounded text-base">
+            {props.icon}
+          </span>
+        )}
+        {props.label}
+      </div>
+      <div className="leading-tight" title={props.title}>
+        {props.value}
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/display/PriceDisplay.tsx
@@ -0,0 +1,25 @@
+type PriceDisplayProps = {
+  badge?: boolean;
+  badgeContent: string;
+  price: number | null;
+  className?: string;
+};
+export function PriceDisplay(props: PriceDisplayProps) {
+  return (
+    <>
+      {props.badge && (
+        <div className="inline-flex px-2 rounded-badge text-slate-400 bg-slate-200 text-sm me-2">
+          {props.badgeContent}
+        </div>
+      )}
+      {props.price ? (
+        <span className={`text-3xl tracking-tighter ${props.className}`}>
+          <span className="text-base">HK $</span>
+          {props.price.toLocaleString("en")}
+        </span>
+      ) : (
+        <span className="text-3xl">Please Offer</span>
+      )}
+    </>
+  );
+}
