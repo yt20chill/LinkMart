4ca4f4d0f45ce5c3454c9358e1aec478602e8b4b
yt20chill <ytelainechiu@gmail.com>
2023-12-14 20:28:26 +0800

    handled data could be undefined

--- frontend/src/pages/public/RequestsPage.tsx
+++ frontend/src/pages/public/RequestsPage.tsx
@@ -1,6 +1,5 @@
 import { RequestCard } from "@/components/card/RequestCard";
 import { RequestCardSkeleton } from "@/components/card/RequestCardSkeleton";
-import { RequestDto } from "@/schemas/responseSchema";
 import { useInfiniteQuery } from "react-query";
 import { Link, useSearchParams } from "react-router-dom";
 import { Filter } from "../../features/filter/Filter";
@@ -11,50 +10,51 @@ import { queryKey } from "../../services/query.config";
 import { RouteEnum, siteMap } from "../../services/routes.config";
 
 function RequestsPage() {
-  const searchParamsWrapper = useSearchParamsWrapper(useSearchParams());
-  const { searchParams } = searchParamsWrapper;
-  const { data: requests, isFetchingNextPage } = useInfiniteQuery({
-    queryKey: [queryKey.REQUEST, { searchParams: searchParams.toString() }],
-    queryFn: ({ pageParam = 1 }) => {
-      searchParams.set("p", (pageParam as number).toString());
-      return getAllRequestsAJAX(searchParams);
-    },
-    getNextPageParam: (lastPage, allPages): number | undefined => {
-      return lastPage && lastPage.length === 0
-        ? allPages.length + 1
-        : undefined;
-    },
-  });
+	const searchParamsWrapper = useSearchParamsWrapper(useSearchParams());
+	const { searchParams } = searchParamsWrapper;
+	const { data: requests, isFetchingNextPage } = useInfiniteQuery({
+		queryKey: [queryKey.REQUEST, { searchParams: searchParams.toString() }],
+		queryFn: ({ pageParam = 1 }) => {
+			searchParams.set("p", (pageParam as number).toString());
+			return getAllRequestsAJAX(searchParams);
+		},
+		getNextPageParam: (lastPage, allPages): number | undefined => {
+			return lastPage && lastPage.length === 0
+				? allPages.length + 1
+				: undefined;
+		},
+	});
 
-  return (
-    <SearchParamsWrapperContext.Provider value={searchParamsWrapper}>
-      <Filter />
-      <div className="mt-5 max-w-7xl max-xl:px-2 mx-auto ">
-        <h1 className="text-black text-xl "></h1>
-      </div>
-      <div className="mt-5 max-w-7xl max-xl:px-2 mx-auto grid gap-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-        {requests ? (
-          requests.pages.map((data) =>
-            data.map((item) => <RequestCard key={item.requestId} {...item} />)
-          )
-        ) : (
-          <RequestCardSkeleton />
-        )}
-      </div>
-      <Link
-        to={siteMap(RouteEnum.PostRequest)}
-        className="fixed text-base-100 bottom-0 right-0 mb-3 mr-3 md:mb-12 md:mr-12"
-      >
-        <div className="flex justify-center items-center bg-gradient-to-r from-amber-300 to-orange-500 p-4 gap-1 rounded-badge shadow">
-          <span className="material-symbols-rounded">add_circle</span>
-          <span className="max-md:hidden">Create Request</span>
-        </div>
-      </Link>
-      {isFetchingNextPage && (
-        <span className="loading loading-dots loading-lg"></span>
-      )}
-    </SearchParamsWrapperContext.Provider>
-  );
+	return (
+		<SearchParamsWrapperContext.Provider value={searchParamsWrapper}>
+			<Filter />
+			<div className="mt-5 max-w-7xl max-xl:px-2 mx-auto ">
+				<h1 className="text-black text-xl "></h1>
+			</div>
+			<div className="mt-5 max-w-7xl max-xl:px-2 mx-auto grid gap-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+				{requests && requests.pages.length > 0 ? (
+					requests.pages.map((data) => {
+						if (!data) return null;
+						data.map((item) => <RequestCard key={item.requestId} {...item} />);
+					})
+				) : (
+					<RequestCardSkeleton />
+				)}
+			</div>
+			<Link
+				to={siteMap(RouteEnum.PostRequest)}
+				className="fixed text-base-100 bottom-0 right-0 mb-3 mr-3 md:mb-12 md:mr-12"
+			>
+				<div className="flex justify-center items-center bg-gradient-to-r from-amber-300 to-orange-500 p-4 gap-1 rounded-badge shadow">
+					<span className="material-symbols-rounded">add_circle</span>
+					<span className="max-md:hidden">Create Request</span>
+				</div>
+			</Link>
+			{isFetchingNextPage && (
+				<span className="loading loading-dots loading-lg"></span>
+			)}
+		</SearchParamsWrapperContext.Provider>
+	);
 }
 
 export default RequestsPage;
