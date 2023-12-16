8d7e9656cfd0772735f695683e42d03552ea310d
yt20chill <ytelainechiu@gmail.com>
2023-12-17 00:11:14 +0800

    test form at home page

--- frontend/src/pages/public/HomePage.tsx
+++ frontend/src/pages/public/HomePage.tsx
@@ -1,5 +1,12 @@
+import PostOfferForm from "../../features/forms/PostOfferForm";
+
 function HomePage() {
-	return <div>HomePage</div>;
+	return (
+		<div>
+			HomePage
+			<PostOfferForm requestId="01BX5ZZKBKACTAV9WEVGEMMVRY" />
+		</div>
+	);
 }
 
 export default HomePage;
