38447948838e5da042db8e4290d69edee63e241b
yt20chill <ytelainechiu@gmail.com>
2023-12-17 12:35:33 +0800

    added get address api backend

--- test-api-backend/src/user/user.controller.ts
+++ test-api-backend/src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
-@Controller('user')
-export class UserController {}
+@Controller('api/user')
+export class UserController {
+  @Get('address')
+  findAllAddress() {
+    return [
+      { addressId: 1, address: 'address 1' },
+      { addressId: 2, address: 'address 2' },
+      { addressId: 3, address: 'address 3' },
+    ];
+  }
+  @Post('address')
+  addAddress() {
+    return { message: 'address added' };
+  }
+}
