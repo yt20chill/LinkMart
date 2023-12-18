ba1071c13103dc0298db6e4512f9a1423b055693
CookedRicer <e.kuquer@gmail.com>
2023-12-18 17:24:37 +0800

    fix imageframe hover possible bug

--- frontend/src/components/imageFrame/MainImageFrame.tsx
+++ frontend/src/components/imageFrame/MainImageFrame.tsx
@@ -9,11 +9,11 @@ type MainImageFrameProps = {
 export function MainImageFrame(props: MainImageFrameProps) {
   return (
     <div
-      className={`flex justify-center aspect-square w-full overflow-hidden rounded-sm bg-slate-300 border border-white/10 ring-1 ring-black/10 ${props.className}`}
+      className={`relative flex justify-center aspect-square w-full overflow-hidden rounded-sm bg-slate-300 border border-white/10 ring-1 ring-black/10 ${props.className}`}
     >
       <img
         title={props.title}
-        className="object-cover origin-top-left"
+        className="object-cover hover:object-contain origin-top-left "
         src={props.imagePath}
         onMouseMove={(e) => imageHoverView(e)}
         onMouseLeave={(e) => imageHoverEnd(e)}
