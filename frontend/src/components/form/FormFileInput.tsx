e423d1a11a7bba371a86572af589f8a9a7802807
CookedRicer <e.kuquer@gmail.com>
2023-12-19 18:08:55 +0800

    change form css

--- frontend/src/components/form/FormFileInput.tsx
+++ frontend/src/components/form/FormFileInput.tsx
@@ -5,36 +5,38 @@ import { BaseFormInputProps } from "../../types/formModels";
 import ErrorMessage from "./ErrorMessage";
 
 type FormFileInputProps<T extends FieldValues> = BaseFormInputProps<T> & {
-	accept?: string;
-	multiple?: boolean;
-	setValue: UseFormSetValue<T>;
+  accept?: string;
+  multiple?: boolean;
+  setValue: UseFormSetValue<T>;
 };
 
 function FormFileInput<T extends FieldValues>({
-	name,
-	register,
-	label = camelToTitleCase(name),
-	accept = allowedFileTypes.join(","),
-	multiple = true,
-	errors,
+  name,
+  register,
+  label = camelToTitleCase(name),
+  accept = allowedFileTypes.join(","),
+  multiple = true,
+  errors,
 }: FormFileInputProps<T>) {
-	const error = errors?.[name]?.message as string | undefined;
+  const error = errors?.[name]?.message as string | undefined;
 
-	return (
-		<label className="form-control w-full max-w-xs">
-			<div className="label">
-				<span className="label-text">{label}</span>
-			</div>
-			<input
-				type="file"
-				multiple={multiple}
-				accept={accept}
-				{...register(name)}
-				className="file-input file-input-bordered w-full max-w-xs"
-			/>
-			{error && <ErrorMessage message={error} />}
-		</label>
-	);
+  return (
+    <label className="form-control w-full max-w-lg h-12 outline-slate-400 outline-2 outline-dashed rounded-btn my-6 py-16 bg-base-100/10 hover:bg-base-100/50 flex items-center justify-center cursor-pointer">
+      <i className="bi bi-images text-slate-400 text-5xl"></i>
+      <div className="text-slate-400">Upload Image</div>
+      <div className="label hidden">
+        <span className="label-text">{label}</span>
+      </div>
+      <input
+        type="file"
+        multiple={multiple}
+        accept={accept}
+        {...register(name)}
+        className="file-input file-input-bordered w-full max-w-lg hidden"
+      />
+      {error && <ErrorMessage message={error} />}
+    </label>
+  );
 }
 
 export default FormFileInput;
--- frontend/src/components/form/FormSelect.tsx
+++ frontend/src/components/form/FormSelect.tsx
@@ -4,44 +4,44 @@ import { BaseFormInputProps, OptionItem } from "../../types/formModels";
 import ErrorMessage from "./ErrorMessage";
 
 type FormSelectProps<T extends FieldValues> = BaseFormInputProps<T> & {
-	optionItems: OptionItem[];
-	required?: boolean;
+  optionItems: OptionItem[];
+  required?: boolean;
 };
 
 const FormSelect = <T extends FieldValues>({
-	name,
-	label = camelToTitleCase(name),
-	placeholder = `Select ${label}`,
-	optionItems,
-	defaultValue,
-	required = true,
-	register,
-	errors,
+  name,
+  label = camelToTitleCase(name),
+  placeholder = `Select ${label}`,
+  optionItems,
+  defaultValue,
+  required = true,
+  register,
+  errors,
 }: FormSelectProps<T>) => {
-	const error = errors?.[name]?.message as string | undefined;
-	return (
-		<label className="form-control w-full max-w-xs">
-			<div className="label">
-				<span className="label-text">{label}</span>
-			</div>
-			<select
-				className="select select-accent w-full max-w-xs"
-				{...register(name)}
-				defaultValue={defaultValue}
-				required={required}
-			>
-				<option disabled value={undefined}>
-					{placeholder}
-				</option>
-				{optionItems.map((item) => (
-					<option key={item.value} value={item.value}>
-						{item.displayValue ?? item.value}
-					</option>
-				))}
-			</select>
-			{error && <ErrorMessage message={error} />}
-		</label>
-	);
+  const error = errors?.[name]?.message as string | undefined;
+  return (
+    <label className="form-control w-full max-w-lg">
+      <div className="label">
+        <span className="label-text">{label}</span>
+      </div>
+      <select
+        className="select select-bordered w-full max-w-lg"
+        {...register(name)}
+        defaultValue={defaultValue}
+        required={required}
+      >
+        <option disabled value={undefined}>
+          {placeholder}
+        </option>
+        {optionItems.map((item) => (
+          <option key={item.value} value={item.value}>
+            {item.displayValue ?? item.value}
+          </option>
+        ))}
+      </select>
+      {error && <ErrorMessage message={error} />}
+    </label>
+  );
 };
 
 export default FormSelect;
