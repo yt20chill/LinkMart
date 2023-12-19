c50c7755ff1cda47f1ab30f401f490c56ab7c2f8
CookedRicer <e.kuquer@gmail.com>
2023-12-19 14:24:18 +0800

    update footer

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -8,7 +8,7 @@ type FooterProps = {
 export function Footer(props: FooterProps) {
   return (
     <div className={`mt-auto bg-slate-700 ${props.className} py-12`}>
-      <div className="mx-auto max-w-7xl grid grid-cols-1 px-6 gap-3 text-slate-300 items-end">
+      <div className="mx-auto max-w-7xl py-3 mb-2 gap-3 text-slate-300 flex justify-center items-end">
         <Link to={siteMap(RouteEnum.Home)}>
           <img
             className="w-48"
@@ -16,9 +16,23 @@ export function Footer(props: FooterProps) {
             title="Linkmart"
           />
         </Link>
-        <div className="col-span-5 text-center text-slate-300 font-light text-sm">
-          Copyright © 2023 Nicolas | Elaine | Fredy | Kenneth
+        <div className="flex gap-5 px-3 mb-1">
+          <a href="https://www.facebook.com/">
+            <i className="bi bi-facebook text-2xl"></i>
+          </a>
+          <a href="https://github.com/">
+            <i className="bi bi-github text-2xl"></i>
+          </a>
+          <a href="https://t.me/">
+            <i className="bi bi-telegram text-2xl"></i>
+          </a>
+          <a href="https://www.instagram.com/">
+            <i className="bi bi-instagram text-2xl"></i>
+          </a>
         </div>
+      </div>
+      <div className="col-span-5 text-center text-slate-300 font-light text-sm">
+        Copyright © 2023 Nicolas | Elaine | Fredy | Kenneth
       </div>
     </div>
   );
