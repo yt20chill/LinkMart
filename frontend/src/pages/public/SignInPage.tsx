1ed99edbf6b6b2f184716b41a0b66c9fbac310e4
CookedRicer <e.kuquer@gmail.com>
2023-12-19 09:50:27 +0800

    enhance css

--- frontend/src/pages/public/SignInPage.tsx
+++ frontend/src/pages/public/SignInPage.tsx
@@ -4,16 +4,19 @@ import { useLoggedInRedirect } from "../../features/hooks/useLoggedInRedirect";
 import { RouteEnum, siteMap } from "../../services/routes.config";
 
 function SignInPage() {
-	useLoggedInRedirect();
-	return (
-		<>
-			<SignInForm />
-			<p className="my-3">
-				New user? <Link to={siteMap(RouteEnum.SignUp)}>Sign up </Link>
-				for free!
-			</p>
-		</>
-	);
+  useLoggedInRedirect();
+  return (
+    <>
+      <SignInForm />
+      <p className="my-3 mx-auto font-roboto text-base tracking-wide [&_a]:hover:text-secondary-500 [&_a]:hover:border-b-4  border-secondary-500">
+        New user?{" "}
+        <Link to={siteMap(RouteEnum.SignUp)} className="text-secondary-300">
+          Sign up{" "}
+        </Link>
+        for free!
+      </p>
+    </>
+  );
 }
 
 export default SignInPage;
