2358e892739a2941095e2856389ac1f14c945fb7
CookedRicer <e.kuquer@gmail.com>
2023-12-19 21:47:57 +0800

    user request layout update

--- frontend/src/pages/user/UserRequestsPage.tsx
+++ frontend/src/pages/user/UserRequestsPage.tsx
@@ -1,23 +1,32 @@
 import { useQuery } from "react-query";
-import { RequestCard } from "../../components/card/RequestCard";
+import { UserRequestCard } from "../../components/card/UserRequestCard";
 import { RequestCardSkeleton } from "../../components/card/RequestCardSkeleton";
 import { getRequestsByUserAJAX } from "../../services/api/userApi";
 import { queryKey } from "../../services/query.config";
 
 const UserRequestsPage = () => {
-	const { data: requests } = useQuery({
-		queryKey: [queryKey.USER, "requests"],
-		queryFn: getRequestsByUserAJAX,
-	});
-	return (
-		<>
-			{requests ? (
-				requests.map((req) => <RequestCard key={req.requestId} {...req} />)
-			) : (
-				<RequestCardSkeleton />
-			)}
-		</>
-	);
+  const { data: requests } = useQuery({
+    queryKey: [queryKey.USER, "requests"],
+    queryFn: getRequestsByUserAJAX,
+  });
+  return (
+    <div className="mt-12 max-w-5xl w-full mx-auto px-6">
+      <div className="flex flex-col mx-auto bg-base-100/50 border border-slate-300 rounded-xl overflow-hidden shadow">
+        <div className="py-4 indent-5 text-slate-500 bg-base-100 border-b border-slate-300">
+          My Request
+        </div>
+        <div className="w-full p-6 min-h-[600px]">
+          {requests ? (
+            requests.map((req) => (
+              <UserRequestCard key={req.requestId} {...req} />
+            ))
+          ) : (
+            <RequestCardSkeleton />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserRequestsPage;
