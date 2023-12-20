383b86521c665ff2cae35b984167f2e3ca02412a
yt20chill <ytelainechiu@gmail.com>
2023-12-20 11:05:14 +0800

    todo: set primary image

--- frontend/src/components/imageFrame/SubImageFrame.tsx
+++ frontend/src/components/imageFrame/SubImageFrame.tsx
@@ -5,13 +5,17 @@ type SubImageFrameProps = {
 
 export function SubImageFrame(props: SubImageFrameProps) {
 	return (
-		<div className="hover:scale-110 hover:z-10 flex justify-center aspect-square overflow-hidden hover:shadow rounded transition-all cursor-pointer bg-slate-100 border border-white/10 ring-1 ring-black/10 hover:ring-offset-2">
+		<div className="[&_nav]:hover:flex hover:scale-110 hover:z-10 flex justify-center aspect-square overflow-hidden hover:shadow rounded transition-all cursor-pointer bg-slate-100 border border-white/10 ring-1 ring-black/10 hover:ring-offset-2">
 			<img
 				title={props.imagePath}
 				className="object-cover"
 				src={props.imagePath}
 				onClick={(e: React.MouseEvent<HTMLImageElement>) => props.onClick(e)}
 			/>
+			{/* TODO: change primary image  */}
+			{/* <nav className="absolute hidden inset-0 bg-base-100/30 backdrop-blur-[1px]">
+				<span className="absolute right-1 top-1 bi bi-star-fill text-slate-600 hover:text-amber-400 drop-shadow text-lg"></span>
+			</nav> */}
 		</div>
 	);
 }
