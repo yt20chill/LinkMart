35bd9338a926ad55efefe8279c88bd9834133f5c
CookedRicer <e.kuquer@gmail.com>
2023-12-20 13:15:57 +0800

    fix capital category name

--- frontend/src/features/forms/CategoryFields.tsx
+++ frontend/src/features/forms/CategoryFields.tsx
@@ -5,65 +5,65 @@ import FormSelect from "../../components/form/FormSelect";
 import { useCategoryOptions } from "../hooks/useCategoryOptions";
 
 type CategoryFieldsFormProps<
-	T extends Record<string, unknown> = Record<string, unknown>
+  T extends Record<string, unknown> = Record<string, unknown>
 > = {
-	keyName: string;
-	register: UseFormRegister<T>;
-	errors: FieldErrors<T>;
-	category: { id: number; name: string };
-	defaultValuesJSON?: Record<string, string>;
+  keyName: string;
+  register: UseFormRegister<T>;
+  errors: FieldErrors<T>;
+  category: { id: number; name: string };
+  defaultValuesJSON?: Record<string, string>;
 };
 
 const CategoryFieldsForm = ({
-	keyName,
-	register,
-	errors,
-	category,
-	defaultValuesJSON,
+  keyName,
+  register,
+  errors,
+  category,
+  defaultValuesJSON,
 }: CategoryFieldsFormProps) => {
-	if (category?.id === undefined) throw new Error("Invalid category id");
-	const { dropDownFields, textFields, defaultEmptyValues } = useCategoryOptions(
-		category.id
-	);
-	const defaultValues = defaultValuesJSON ?? defaultEmptyValues;
-	return (
-		<div className="mt-3">
-			{(dropDownFields.length > 0 || textFields.length > 0) && (
-				<div className="inline-flex border-b-8  border-slate-300 text-3xl font-bold text-slate-500">
-					{category.name} Options
-				</div>
-			)}
-			<div className="">
-				{dropDownFields.map((field) => {
-					const fieldName = Object.keys(field)[0];
-					const fieldOptions = field[fieldName];
-					return (
-						<FormSelect
-							key={fieldName}
-							name={`${keyName}.${fieldName}`}
-							label={camelToTitleCase(fieldName)}
-							register={register}
-							errors={errors}
-							optionItems={fieldOptions.map((option) => ({
-								value: option,
-							}))}
-							defaultValue={defaultValues[fieldName] ?? ""}
-						/>
-					);
-				})}
-				{textFields.map((fieldName) => (
-					<FormInput
-						key={fieldName}
-						name={`${keyName}.${fieldName}`}
-						label={camelToTitleCase(fieldName)}
-						register={register}
-						errors={errors}
-						defaultValue={defaultValues[fieldName] ?? ""}
-					/>
-				))}
-			</div>
-		</div>
-	);
+  if (category?.id === undefined) throw new Error("Invalid category id");
+  const { dropDownFields, textFields, defaultEmptyValues } = useCategoryOptions(
+    category.id
+  );
+  const defaultValues = defaultValuesJSON ?? defaultEmptyValues;
+  return (
+    <div className="mt-4">
+      {(dropDownFields.length > 0 || textFields.length > 0) && (
+        <div className="inline-flex border-b-8  border-slate-300 text-xl font-bold text-slate-500">
+          {camelToTitleCase(category.name)} Options
+        </div>
+      )}
+      <div className="">
+        {dropDownFields.map((field) => {
+          const fieldName = Object.keys(field)[0];
+          const fieldOptions = field[fieldName];
+          return (
+            <FormSelect
+              key={fieldName}
+              name={`${keyName}.${fieldName}`}
+              label={camelToTitleCase(fieldName)}
+              register={register}
+              errors={errors}
+              optionItems={fieldOptions.map((option) => ({
+                value: option,
+              }))}
+              defaultValue={defaultValues[fieldName] ?? ""}
+            />
+          );
+        })}
+        {textFields.map((fieldName) => (
+          <FormInput
+            key={fieldName}
+            name={`${keyName}.${fieldName}`}
+            label={camelToTitleCase(fieldName)}
+            register={register}
+            errors={errors}
+            defaultValue={defaultValues[fieldName] ?? ""}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryFieldsForm;
--- frontend/src/features/forms/PostRequestForm.tsx
+++ frontend/src/features/forms/PostRequestForm.tsx
@@ -5,258 +5,259 @@ import { useMutation, useQueryClient } from "react-query";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import {
-	FormInput,
-	FormSelect,
-	FormSubmitButton,
-	ImagePreview,
+  FormInput,
+  FormSelect,
+  FormSubmitButton,
+  ImagePreview,
 } from "../../components/form";
 import FormFileInput from "../../components/form/FormFileInput";
 import CancelButton from "../../components/ui/CancelButton";
 import { appendFormData } from "../../lib/formUtils";
 import {
-	RequestForm,
-	allowedFileTypes,
-	postRequestSchema,
+  RequestForm,
+  allowedFileTypes,
+  postRequestSchema,
 } from "../../schemas/requestSchema";
 import { ImageDto } from "../../schemas/responseSchema";
 import {
-	cloneRequestAJAX,
-	deleteRequestImageAJAX,
-	postRequestAJAX,
-	putRequestAJAX,
+  cloneRequestAJAX,
+  deleteRequestImageAJAX,
+  postRequestAJAX,
+  putRequestAJAX,
 } from "../../services/api/requestApi";
 import { queryKey } from "../../services/query.config";
 import { RouteEnum, siteMap } from "../../services/routes.config";
 import { usePreviewFormImages } from "../hooks/usePreviewFormImages";
 import { useQueryContainer } from "../hooks/useQueryContainer";
 import {
-	RequestFormTextFields,
-	useUpdateRequestForm,
+  RequestFormTextFields,
+  useUpdateRequestForm,
 } from "../hooks/useUpdateForm";
 import CategoryFieldsForm from "./CategoryFields";
 import SkeletonForm from "./SkeletonForm";
+import { camelToTitleCase, textToTitleCase } from "@/lib/utils";
 
 const PostRequestForm = () => {
-	const [searchParams] = useSearchParams();
-	const requestId =
-		searchParams.get("cloneId") || searchParams.get("requestId");
-	const isClone = searchParams.has("cloneId");
-	const { defaultValuesByField, images, primaryImage } =
-		useUpdateRequestForm(requestId);
-	const [imagesClone, setImagesClone] = useState<Array<ImageDto>>(images);
-	const defaultValues = useMemo(
-		() => ({
-			...defaultValuesByField.text,
-			...defaultValuesByField.dropDown,
-		}),
-		[defaultValuesByField]
-	);
-	const { categories, locations } = useQueryContainer();
-	const queryClient = useQueryClient();
-	const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const requestId =
+    searchParams.get("cloneId") || searchParams.get("requestId");
+  const isClone = searchParams.has("cloneId");
+  const { defaultValuesByField, images, primaryImage } =
+    useUpdateRequestForm(requestId);
+  const [imagesClone, setImagesClone] = useState<Array<ImageDto>>(images);
+  const defaultValues = useMemo(
+    () => ({
+      ...defaultValuesByField.text,
+      ...defaultValuesByField.dropDown,
+    }),
+    [defaultValuesByField]
+  );
+  const { categories, locations } = useQueryContainer();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
-	// Create, clone or edit request
-	const { mutateAsync: mutateRequest, isLoading } = useMutation({
-		mutationFn: (formData: FormData) => {
-			if (isClone) return cloneRequestAJAX(formData);
-			if (requestId) return putRequestAJAX(requestId, formData);
-			return postRequestAJAX(formData);
-		},
-		onSuccess: async (result) => {
-			if (!result) return toast.error("Something went wrong");
-			toast.success("Request posted!");
-			await queryClient.invalidateQueries([queryKey.REQUEST]);
-			navigate(`${siteMap(RouteEnum.RequestDetail)}/${result.requestId}`, {
-				replace: true,
-			});
-		},
-	});
+  // Create, clone or edit request
+  const { mutateAsync: mutateRequest, isLoading } = useMutation({
+    mutationFn: (formData: FormData) => {
+      if (isClone) return cloneRequestAJAX(formData);
+      if (requestId) return putRequestAJAX(requestId, formData);
+      return postRequestAJAX(formData);
+    },
+    onSuccess: async (result) => {
+      if (!result) return toast.error("Something went wrong");
+      toast.success("Request posted!");
+      await queryClient.invalidateQueries([queryKey.REQUEST]);
+      navigate(`${siteMap(RouteEnum.RequestDetail)}/${result.requestId}`, {
+        replace: true,
+      });
+    },
+  });
 
-	// Owner delete images
-	const { mutateAsync: deleteImage } = useMutation({
-		mutationFn: deleteRequestImageAJAX,
-		onSuccess: async () => {
-			toast.success("image deleted");
-			await queryClient.invalidateQueries([queryKey.REQUEST, { requestId }]);
-		},
-	});
+  // Owner delete images
+  const { mutateAsync: deleteImage } = useMutation({
+    mutationFn: deleteRequestImageAJAX,
+    onSuccess: async () => {
+      toast.success("image deleted");
+      await queryClient.invalidateQueries([queryKey.REQUEST, { requestId }]);
+    },
+  });
 
-	// Form
-	const {
-		register,
-		handleSubmit,
-		watch,
-		formState: { errors },
-		setValue,
-		reset,
-	} = useForm<RequestForm>({
-		resolver: zodResolver(
-			requestId
-				? postRequestSchema.omit({ imageFile: true })
-				: postRequestSchema
-		),
-		defaultValues,
-		mode: "onSubmit",
-	});
+  // Form
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    setValue,
+    reset,
+  } = useForm<RequestForm>({
+    resolver: zodResolver(
+      requestId
+        ? postRequestSchema.omit({ imageFile: true })
+        : postRequestSchema
+    ),
+    defaultValues,
+    mode: "onSubmit",
+  });
 
-	// reset form on defaultValue change
-	useEffect(() => {
-		reset(defaultValues);
-	}, [defaultValues, reset]);
+  // reset form on defaultValue change
+  useEffect(() => {
+    reset(defaultValues);
+  }, [defaultValues, reset]);
 
-	// initialize imagesClone when images change
-	useEffect(() => {
-		if (images.length > 0) setImagesClone(images);
-	}, [images]);
+  // initialize imagesClone when images change
+  useEffect(() => {
+    if (images.length > 0) setImagesClone(images);
+  }, [images]);
 
-	// set delete images functions
-	const { base64Images, onDelete } = usePreviewFormImages<RequestForm>(
-		watch,
-		"imageFile",
-		setValue
-	);
-	const removeImageFromArray = ({ imageId }: { imageId: number }) =>
-		setImagesClone((prev) => prev.filter((img) => img.imageId !== imageId));
-	const onDeleteExisting = isClone ? removeImageFromArray : deleteImage;
+  // set delete images functions
+  const { base64Images, onDelete } = usePreviewFormImages<RequestForm>(
+    watch,
+    "imageFile",
+    setValue
+  );
+  const removeImageFromArray = ({ imageId }: { imageId: number }) =>
+    setImagesClone((prev) => prev.filter((img) => img.imageId !== imageId));
+  const onDeleteExisting = isClone ? removeImageFromArray : deleteImage;
 
-	// watch categoryId to display corresponding fields
-	const categoryId = +watch("categoryId");
+  // watch categoryId to display corresponding fields
+  const categoryId = +watch("categoryId");
 
-	// reset itemDetail when categoryId changes
-	useEffect(() => {
-		setValue("itemDetail", {});
-	}, [categoryId, setValue]);
+  // reset itemDetail when categoryId changes
+  useEffect(() => {
+    setValue("itemDetail", {});
+  }, [categoryId, setValue]);
 
-	const onSubmit = async (data: RequestForm) => {
-		const formData = new FormData();
-		appendFormData(data, formData);
-		if (isClone)
-			appendFormData(
-				{
-					imageUrl: imagesClone.map((img) => img.imagePath),
-				},
-				formData
-			);
-		await mutateRequest(formData);
-	};
-	return (
-		<>
-			{categories && locations ? (
-				<form className="max-w-lg mx-auto p-12">
-					{categories && (
-						<FormSelect
-							register={register}
-							required={true}
-							errors={errors}
-							name="categoryId"
-							label="Category"
-							defaultValue={defaultValuesByField.dropDown.categoryId}
-							optionItems={categories.map((category) => ({
-								value: category.categoryId + "",
-								displayValue: category.categoryName,
-							}))}
-						/>
-					)}
-					{locations && (
-						<FormSelect
-							register={register}
-							required={true}
-							errors={errors}
-							name="locationId"
-							label="Location"
-							defaultValue={defaultValuesByField.dropDown.locationId}
-							optionItems={locations.map((location) => ({
-								value: location.locationId + "",
-								displayValue: location.locationName,
-							}))}
-						/>
-					)}
-					<FormFileInput
-						name="imageFile"
-						register={register}
-						setValue={setValue}
-						errors={errors}
-						multiple={true}
-						accept={allowedFileTypes.join(",")}
-					/>
-					<div className="grid grid-cols-5 gap-1">
-						{primaryImage && (
-							<ImagePreview
-								name={primaryImage}
-								src={primaryImage}
-								canDelete={false}
-								onDelete={() => {}}
-							/>
-						)}
-						{base64Images.length > 0 &&
-							base64Images
-								.reverse()
-								.map((img) => (
-									<ImagePreview key={img.name} {...img} onDelete={onDelete} />
-								))}
-						{imagesClone.length > 0 &&
-							imagesClone
-								.reverse()
-								.map((img) => (
-									<ImagePreview
-										key={img.imageId}
-										imageId={img.imageId}
-										src={img.imagePath}
-										onDelete={onDeleteExisting}
-									/>
-								))}
-					</div>
+  const onSubmit = async (data: RequestForm) => {
+    const formData = new FormData();
+    appendFormData(data, formData);
+    if (isClone)
+      appendFormData(
+        {
+          imageUrl: imagesClone.map((img) => img.imagePath),
+        },
+        formData
+      );
+    await mutateRequest(formData);
+  };
+  return (
+    <>
+      {categories && locations ? (
+        <form className="max-w-lg mx-auto p-12">
+          {categories && (
+            <FormSelect
+              register={register}
+              required={true}
+              errors={errors}
+              name="categoryId"
+              label="Category"
+              defaultValue={defaultValuesByField.dropDown.categoryId}
+              optionItems={categories.map((category) => ({
+                value: category.categoryId + "",
+                displayValue: textToTitleCase(category.categoryName),
+              }))}
+            />
+          )}
+          {locations && (
+            <FormSelect
+              register={register}
+              required={true}
+              errors={errors}
+              name="locationId"
+              label="Location"
+              defaultValue={defaultValuesByField.dropDown.locationId}
+              optionItems={locations.map((location) => ({
+                value: location.locationId + "",
+                displayValue: location.locationName,
+              }))}
+            />
+          )}
+          <FormFileInput
+            name="imageFile"
+            register={register}
+            setValue={setValue}
+            errors={errors}
+            multiple={true}
+            accept={allowedFileTypes.join(",")}
+          />
+          <div className="grid grid-cols-5 gap-1">
+            {primaryImage && (
+              <ImagePreview
+                name={primaryImage}
+                src={primaryImage}
+                canDelete={false}
+                onDelete={() => {}}
+              />
+            )}
+            {base64Images.length > 0 &&
+              base64Images
+                .reverse()
+                .map((img) => (
+                  <ImagePreview key={img.name} {...img} onDelete={onDelete} />
+                ))}
+            {imagesClone.length > 0 &&
+              imagesClone
+                .reverse()
+                .map((img) => (
+                  <ImagePreview
+                    key={img.imageId}
+                    imageId={img.imageId}
+                    src={img.imagePath}
+                    onDelete={onDeleteExisting}
+                  />
+                ))}
+          </div>
 
-					{Object.keys(defaultValuesByField.text).map((field) => (
-						<FormInput
-							key={field}
-							name={field as keyof RequestForm}
-							defaultValue={
-								defaultValuesByField.text[field as keyof RequestFormTextFields]
-							}
-							register={register}
-							errors={errors}
-						/>
-					))}
-					{categoryId && (
-						<CategoryFieldsForm
-							keyName="itemDetail"
-							register={
-								register as unknown as UseFormRegister<Record<string, unknown>>
-							}
-							errors={errors as FieldErrors<Record<string, string>>}
-							category={{
-								id: categoryId,
-								name:
-									categories.find(
-										(category) => category.categoryId === categoryId
-									)?.categoryName ?? "",
-							}}
-							defaultValuesJSON={
-								defaultValuesByField.others.itemDetail ?? undefined
-							}
-						/>
-					)}
-					<div className="flex justify-end mt-8 gap-2">
-						<FormSubmitButton
-							label="Create Post"
-							onClick={handleSubmit(onSubmit)}
-							disabled={isLoading}
-						/>
-						<CancelButton
-							label="Cancel"
-							onClick={(e) => {
-								e.preventDefault();
-								navigate(-1);
-							}}
-						/>
-					</div>
-				</form>
-			) : (
-				<SkeletonForm />
-			)}
-		</>
-	);
+          {Object.keys(defaultValuesByField.text).map((field) => (
+            <FormInput
+              key={field}
+              name={field as keyof RequestForm}
+              defaultValue={
+                defaultValuesByField.text[field as keyof RequestFormTextFields]
+              }
+              register={register}
+              errors={errors}
+            />
+          ))}
+          {categoryId && (
+            <CategoryFieldsForm
+              keyName="itemDetail"
+              register={
+                register as unknown as UseFormRegister<Record<string, unknown>>
+              }
+              errors={errors as FieldErrors<Record<string, string>>}
+              category={{
+                id: categoryId,
+                name:
+                  categories.find(
+                    (category) => category.categoryId === categoryId
+                  )?.categoryName ?? "",
+              }}
+              defaultValuesJSON={
+                defaultValuesByField.others.itemDetail ?? undefined
+              }
+            />
+          )}
+          <div className="flex justify-end mt-8 gap-2">
+            <FormSubmitButton
+              label="Create Post"
+              onClick={handleSubmit(onSubmit)}
+              disabled={isLoading}
+            />
+            <CancelButton
+              label="Cancel"
+              onClick={(e) => {
+                e.preventDefault();
+                navigate(-1);
+              }}
+            />
+          </div>
+        </form>
+      ) : (
+        <SkeletonForm />
+      )}
+    </>
+  );
 };
 
 export default PostRequestForm;
