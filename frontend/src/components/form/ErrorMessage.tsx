3658dcef0ca72e3b7111e50683727c3eb048a535
CookedRicer <e.kuquer@gmail.com>
2023-12-21 14:07:12 +0800

    style error msg

--- frontend/src/components/form/ErrorMessage.tsx
+++ frontend/src/components/form/ErrorMessage.tsx
@@ -3,24 +3,14 @@ type ErrorMessageProps = {
 };
 function ErrorMessage({ message }: ErrorMessageProps) {
   return (
-    <div
-      role="alert"
-      className="alert alert-error p-2 mt-2 flex items-start rounded-lg bg-red-500 text-sm"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="stroke-current shrink-0 h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
+    <div className="mt-2">
+      <div
+        role="alert"
+        className="px-2  gap-2 text-red-500 border border-red-500 pe-8 flex items-center rounded-badge text-sm overflow-hidden bg-red-100/80 backdrop-blur-lg"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-        />
-      </svg>
-      <span>{message}</span>
+        <i className="bi bi-x text-xl max-h-8"></i>
+        <span>{message}</span>
+      </div>
     </div>
   );
 }
