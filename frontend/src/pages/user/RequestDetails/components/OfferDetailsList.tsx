7a373a9a706e2b54e6dd171c8a4f28ad6f44b950
yt20chill <ytelainechiu@gmail.com>
2023-12-21 10:51:40 +0800

    rename uppercase folder

--- frontend/src/pages/user/RequestDetails/components/OfferDetailsList.tsx
+++ frontend/src/pages/user/RequestDetails/components/OfferDetailsList.tsx
@@ -3,8 +3,8 @@ import { toast } from "react-toastify";
 import Skeleton from "../../../../components/skeletons/Skeleton";
 import { RequestId } from "../../../../schemas/requestSchema";
 import {
-  declineOfferAJAX,
-  getAllOffersByRequestIdAJAX,
+	declineOfferAJAX,
+	getAllOffersByRequestIdAJAX,
 } from "../../../../services/api/offerApi";
 import { OfferDetailsContext } from "../../../../services/context/OfferDetailsContext";
 import { queryKey } from "../../../../services/query.config";
@@ -13,40 +13,40 @@ import OfferDetails from "./OfferDetails";
 type OfferDetailsListProps = RequestId;
 
 const OfferDetailsList = ({ requestId }: OfferDetailsListProps) => {
-  const { data: offers, isLoading: isGettingOffers } = useQuery({
-    queryKey: [queryKey.OFFER, { requestId }],
-    queryFn: () => getAllOffersByRequestIdAJAX({ requestId }),
-  });
-  const queryClient = useQueryClient();
-  const { mutateAsync: onDecline } = useMutation({
-    mutationFn: declineOfferAJAX,
-    onSuccess: async () => {
-      toast.success("Offer has been declined");
-      await queryClient.invalidateQueries([queryKey.OFFER, { requestId }]);
-    },
-  });
+	const { data: offers, isLoading: isGettingOffers } = useQuery({
+		queryKey: [queryKey.OFFER, { requestId }],
+		queryFn: () => getAllOffersByRequestIdAJAX({ requestId }),
+	});
+	const queryClient = useQueryClient();
+	const { mutateAsync: onDecline } = useMutation({
+		mutationFn: declineOfferAJAX,
+		onSuccess: async () => {
+			toast.success("Offer has been declined");
+			await queryClient.invalidateQueries([queryKey.OFFER, { requestId }]);
+		},
+	});
 
-  return (
-    <>
-      {isGettingOffers && <Skeleton />}
+	return (
+		<>
+			{isGettingOffers && <Skeleton />}
 
-      {offers && (
-        <OfferDetailsContext.Provider
-          value={{ offersDetails: offers, onDecline }}
-        >
-          {offers.length > 0 ? (
-            offers.map((offer) => (
-              <OfferDetails key={offer.offerId} offerId={offer.offerId} />
-            ))
-          ) : (
-            <div className="flex items-center justify-center text-gray-300 py-12">
-              No Offer
-            </div>
-          )}
-        </OfferDetailsContext.Provider>
-      )}
-    </>
-  );
+			{offers && (
+				<OfferDetailsContext.Provider
+					value={{ offersDetails: offers, onDecline }}
+				>
+					{offers.length > 0 ? (
+						offers.map((offer) => (
+							<OfferDetails key={offer.offerId} offerId={offer.offerId} />
+						))
+					) : (
+						<div className="flex items-center justify-center text-gray-300 py-12">
+							No Offer
+						</div>
+					)}
+				</OfferDetailsContext.Provider>
+			)}
+		</>
+	);
 };
 
 export default OfferDetailsList;
