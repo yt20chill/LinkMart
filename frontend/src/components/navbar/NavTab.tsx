1a5818822c6cec71868c6c8ab81c9cd342445259
CookedRicer <e.kuquer@gmail.com>
2023-12-21 11:10:28 +0800

    navTab merge

--- frontend/src/components/navbar/NavTab.tsx
+++ frontend/src/components/navbar/NavTab.tsx
@@ -2,36 +2,36 @@ import { useNavigate } from "react-router-dom";
 import { TabContextType } from "../../services/context/TabsContext";
 
 type NavTabProps<T extends string[]> = {
-	tabs: Readonly<Record<T[number], string>>;
-	useTabContext: () => TabContextType<T[number]>;
+  tabs: Readonly<Record<T[number], string>>;
+  useTabContext: () => TabContextType<T[number]>;
 };
 
 const NavTab = <T extends string[]>({
-	tabs,
-	useTabContext,
+  tabs,
+  useTabContext,
 }: NavTabProps<T>) => {
-	const { activeTab, setActiveTab } = useTabContext();
-	const navigate = useNavigate();
+  const { activeTab, setActiveTab } = useTabContext();
+  const navigate = useNavigate();
 
-	return (
-		<div className="flex flex-col w-96 gap-2 bg-base-100 grow">
-			{Object.keys(tabs).map((tab) => (
-				<div
-					key={tab}
-					className={`flex justify-center items-center px-12 py-6 hover:bg-primary-400 ${
-						activeTab === tab ? "bg-primary-400" : ""
-					}`}
-					onClick={(e) => {
-						e.preventDefault();
-						setActiveTab(tab);
-						navigate(tabs[tab as keyof typeof tabs]);
-					}}
-				>
-					{tab}
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className="flex flex-col w-96 gap-2 bg-base-100 grow">
+      {Object.keys(tabs).map((tab) => (
+        <div
+          key={tab}
+          className={`flex justify-center items-center px-12 py-6 hover:bg-primary-400 ${
+            activeTab === tab ? "bg-primary-400" : ""
+          }`}
+          onClick={(e) => {
+            e.preventDefault();
+            setActiveTab(tab);
+            navigate(tabs[tab as keyof typeof tabs]);
+          }}
+        >
+          {tab}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default NavTab;
--- frontend/src/components/navbar/ProviderNavTab.tsx
+++ frontend/src/components/navbar/ProviderNavTab.tsx
@@ -1,26 +1,26 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import {
-	ProviderTabContext,
-	TabContextType,
-	useProviderTabContext,
+  ProviderTabContext,
+  TabContextType,
+  useProviderTabContext,
 } from "../../services/context/TabsContext";
 import { ProviderTabs, providerTabs } from "../../services/routes.config";
 import NavTab from "./NavTab";
 
 const ProviderLayout = () => {
-	const [activeTab, setActiveTab] = useState<ProviderTabs>("Offers");
-	return (
-		<>
-			<ProviderTabContext.Provider value={{ activeTab, setActiveTab }}>
-				<NavTab
-					tabs={providerTabs}
-					useTabContext={useProviderTabContext as () => TabContextType<string>}
-				/>
-				<Outlet />
-			</ProviderTabContext.Provider>
-		</>
-	);
+  const [activeTab, setActiveTab] = useState<ProviderTabs>("Offers");
+  return (
+    <>
+      <ProviderTabContext.Provider value={{ activeTab, setActiveTab }}>
+        <NavTab
+          tabs={providerTabs}
+          useTabContext={useProviderTabContext as () => TabContextType<string>}
+        />
+        <Outlet />
+      </ProviderTabContext.Provider>
+    </>
+  );
 };
 
 export default ProviderLayout;
