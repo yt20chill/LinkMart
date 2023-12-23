22f46c054349b4df4dda50b8ab04b9476c597244
yt20chill <ytelainechiu@gmail.com>
2023-12-23 18:49:42 +0800

    inital state get from url

--- frontend/src/components/navbar/ProviderNavTab.tsx
+++ frontend/src/components/navbar/ProviderNavTab.tsx
@@ -1,26 +1,33 @@
 import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { ignoreCaseAndPlural } from "../../lib/formattingUtils";
 import {
-  ProviderTabContext,
-  TabContextType,
-  useProviderTabContext,
+	ProviderTabContext,
+	TabContextType,
+	useProviderTabContext,
 } from "../../services/context/TabsContext";
 import { ProviderTabs, providerTabs } from "../../services/routes.config";
 import NavTab from "./NavTab";
 
 const ProviderLayout = () => {
-  const [activeTab, setActiveTab] = useState<ProviderTabs>("Offers");
-  return (
-    <>
-      <ProviderTabContext.Provider value={{ activeTab, setActiveTab }}>
-        <NavTab
-          tabs={providerTabs}
-          useTabContext={useProviderTabContext as () => TabContextType<string>}
-        />
-        <Outlet />
-      </ProviderTabContext.Provider>
-    </>
-  );
+	const lastParam = useLocation().pathname.split("/").at(-1) ?? "";
+	const tab =
+		ignoreCaseAndPlural<ProviderTabs>(
+			lastParam,
+			Object.keys(providerTabs) as ProviderTabs[]
+		) ?? (Object.keys(providerTabs)[0] as ProviderTabs);
+	const [activeTab, setActiveTab] = useState<ProviderTabs>(tab);
+	return (
+		<>
+			<ProviderTabContext.Provider value={{ activeTab, setActiveTab }}>
+				<NavTab
+					tabs={providerTabs}
+					useTabContext={useProviderTabContext as () => TabContextType<string>}
+				/>
+				<Outlet />
+			</ProviderTabContext.Provider>
+		</>
+	);
 };
 
 export default ProviderLayout;
