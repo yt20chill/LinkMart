fdd2f02bdba75bd88dc51cd88d467ee9ee31563c
CookedRicer <e.kuquer@gmail.com>
2023-12-24 23:16:06 +0800

    edit provider profile

--- frontend/src/features/forms/ProviderProfileForm.tsx
+++ frontend/src/features/forms/ProviderProfileForm.tsx
@@ -5,45 +5,58 @@ import { toast } from "react-toastify";
 import { FormSubmitButton, FormTextAreaInput } from "../../components/form";
 import { generateDefaultValues } from "../../lib/formUtils";
 import {
-	EditProviderProfileForm,
-	editProviderProfileSchema,
+  EditProviderProfileForm,
+  editProviderProfileSchema,
 } from "../../schemas/requestSchema";
 import { editProviderProfileAJAX } from "../../services/api/providerApi";
 import { queryKey } from "../../services/query.config";
 
 type ProviderProfileFormProps = {
-	defaultValues?: EditProviderProfileForm;
+  defaultValues?: EditProviderProfileForm;
 };
 
 const ProviderProfileForm = ({
-	defaultValues = generateDefaultValues(editProviderProfileSchema),
+  defaultValues = generateDefaultValues(editProviderProfileSchema),
 }: ProviderProfileFormProps) => {
-	const {
-		handleSubmit,
-		register,
-		formState: { errors },
-	} = useForm<EditProviderProfileForm>({
-		resolver: zodResolver(editProviderProfileSchema),
-		defaultValues,
-		mode: "onBlur",
-	});
-	const queryClient = useQueryClient();
-	const { mutateAsync: edit, isLoading } = useMutation({
-		mutationFn: editProviderProfileAJAX,
-		onSuccess: async () => {
-			toast.success("Profile updated successfully");
-			await queryClient.invalidateQueries([queryKey.PROVIDER, "profile"]);
-		},
-	});
-	const onSubmit = async (form: EditProviderProfileForm) => {
-		await edit(form);
-	};
-	return (
-		<form>
-			<FormTextAreaInput name="biography" register={register} errors={errors} />
-			<FormSubmitButton onClick={handleSubmit(onSubmit)} disabled={isLoading} />
-		</form>
-	);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<EditProviderProfileForm>({
+    resolver: zodResolver(editProviderProfileSchema),
+    defaultValues,
+    mode: "onBlur",
+  });
+  const queryClient = useQueryClient();
+  const { mutateAsync: edit, isLoading } = useMutation({
+    mutationFn: editProviderProfileAJAX,
+    onSuccess: async () => {
+      toast.success("Profile updated successfully");
+      await queryClient.invalidateQueries([queryKey.PROVIDER, "profile"]);
+    },
+  });
+  const onSubmit = async (form: EditProviderProfileForm) => {
+    await edit(form);
+  };
+  return (
+    <>
+      <h2 className="text-xl inline-flex gap-2 text-primary-400 font-bold p-6 pb-3 border-b border-primary-400 shadow-3xl">
+        <i className="bi bi-chat-square-text"></i>
+        Edit Biography
+      </h2>
+      <form className="p-8 pt-4 flex flex-col gap-6">
+        <FormTextAreaInput
+          name="biography"
+          register={register}
+          errors={errors}
+        />
+        <FormSubmitButton
+          onClick={handleSubmit(onSubmit)}
+          disabled={isLoading}
+        />
+      </form>
+    </>
+  );
 };
 
 export default ProviderProfileForm;
--- frontend/src/pages/user/requestDetails/components/Rating.tsx
+++ frontend/src/pages/user/requestDetails/components/Rating.tsx
@@ -2,42 +2,58 @@ import { maskHalfClassNameArr } from "../../../../lib/formUtils";
 import { camelToTitleCase } from "../../../../lib/utils";
 
 type RatingProps = {
-	name: string;
-	label?: string;
-	score: number;
-	maxScore?: number;
-	readOnly?: boolean;
+  size?: string;
+  name: string;
+  label?: string;
+  score: number;
+  maxScore?: number;
+  readOnly?: boolean;
 };
 
 const Rating = ({
-	name,
-	label = camelToTitleCase(name),
-	score,
-	maxScore = 5,
-	readOnly = name === "",
+  size,
+  name,
+  label = camelToTitleCase(name),
+  score,
+  maxScore = 5,
+  readOnly = name === "",
 }: RatingProps) => {
-	return (
-		<>
-			<span>{label}</span>
-			<div className="rating rating-sm rating-half">
-				<input type="radio" name={name} className="rating-hidden hidden" />
-				{Array(maxScore * 2)
-					.fill(null)
-					.map((_, index) => (
-						<input
-							className={`mask mask-star-2 bg-orange-400 ${
-								maskHalfClassNameArr[index % 2]
-							}`}
-							key={index}
-							type="radio"
-							name={name}
-							checked={Math.floor(score * 2) === index + 1}
-							readOnly={readOnly}
-						/>
-					))}
-			</div>
-		</>
-	);
+  const starSize = () => {
+    switch (size) {
+      case "lg":
+        return "rating-lg";
+      case "md":
+        return "rating-md";
+      case "sm":
+        return "rating-sm";
+      case "xs":
+        return "rating-xs";
+      default:
+        return "rating-sm";
+    }
+  };
+  return (
+    <>
+      <span>{label}</span>
+      <div className={`rating rating-half ${starSize()}`}>
+        <input type="radio" name={name} className="rating-hidden hidden" />
+        {Array(maxScore * 2)
+          .fill(null)
+          .map((_, index) => (
+            <input
+              className={`mask mask-star-2 bg-orange-400 ${
+                maskHalfClassNameArr[index % 2]
+              }`}
+              key={index}
+              type="radio"
+              name={name}
+              checked={Math.floor(score * 2) === index + 1}
+              readOnly={readOnly}
+            />
+          ))}
+      </div>
+    </>
+  );
 };
 
 export default Rating;
