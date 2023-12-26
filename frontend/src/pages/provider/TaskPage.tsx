e2646a162587abb597ebd5b9eee339321664f598
CookedRicer <e.kuquer@gmail.com>
2023-12-26 15:44:30 +0800

    task/order mob resp

--- frontend/src/pages/provider/TaskPage.tsx
+++ frontend/src/pages/provider/TaskPage.tsx
@@ -14,8 +14,8 @@ const TaskPage = () => {
     <OrderStatusTabContext.Provider
       value={{ activeTab: tab, setActiveTab: setTab }}
     >
-      <div className="mt-12 max-w-5xl w-full flex flex-col mx-auto px-6">
-        <div className="flex flex-col bg-base-100/50 border border-slate-500/20 rounded-xl overflow-hidden shadow">
+      <div className="mt-6 sm:mt-12 max-w-5xl w-full flex flex-col mx-auto sm:px-6">
+        <div className="flex flex-col bg-base-100/50 mx-0 sm:mx-2 sm:border border-slate-500/20 rounded-xl overflow-hidden sm:shadow">
           <div className="flex w-full pt-4 text-slate-500 bg-base-100 border-slate-300 ring-b-[4px]">
             {orderStatusTabs.map((tab) => (
               <Tab
