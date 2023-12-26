8b694668dc9ba326e7e0a6f3f4e64f3ac3267420
yt20chill <ytelainechiu@gmail.com>
2023-12-26 23:05:40 +0800

    post or edit offer will not navigate to prev page on success

--- frontend/src/features/forms/OfferForm.tsx
+++ frontend/src/features/forms/OfferForm.tsx
@@ -1,7 +1,6 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "react-query";
-import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { ErrorMessage, FormInput } from "../../components/form";
 import FormSubmitButton from "../../components/form/FormSubmitButton";
@@ -10,88 +9,94 @@ import { FetchError } from "../../lib/apiUtils";
 import { dtoToString, generateDefaultValues } from "../../lib/formUtils";
 import { TOfferForm, offerSchema } from "../../schemas/requestSchema";
 import { postOfferAJAX, putOfferAJAX } from "../../services/api/offerApi";
+import { useControlModalContext } from "../../services/context/ControlModalContext";
 import { queryKey } from "../../services/query.config";
 
 const defaultEmptyValues = generateDefaultValues(offerSchema);
 
 type TPostOfferFormProps = {
-  requestId: string;
+	requestId: string;
 };
 type TPutOfferFormProps = {
-  offerId: string;
-  defaultValues: TOfferForm;
+	offerId: string;
+	defaultValues: TOfferForm;
 };
 
 type OfferFormProps = TPostOfferFormProps | TPutOfferFormProps;
 
 const OfferForm = (props: OfferFormProps) => {
-  const isEdit = "offerId" in props;
-  const id = isEdit ? props.offerId : props.requestId;
-  const defaultValues = isEdit
-    ? dtoToString(props.defaultValues)
-    : defaultEmptyValues;
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<TOfferForm>({
-    resolver: zodResolver(offerSchema),
-    defaultValues,
-  });
-  const queryClient = useQueryClient();
-  const navigate = useNavigate();
-  const {
-    mutateAsync: mutateOffer,
-    error,
-    isLoading,
-  } = useMutation({
-    mutationFn: (data: TOfferForm) =>
-      "requestId" in props ? postOfferAJAX(id, data) : putOfferAJAX(id, data),
-    onSuccess: async () => {
-      toast.success(`Offer has been made successfully!`);
-      await queryClient.invalidateQueries(queryKey.OFFER);
-      navigate(-1);
-    },
-  });
-  const onSubmit = async (form: TOfferForm) => {
-    await mutateOffer(form);
-  };
-  return (
-    <>
-      <h2 className="text-xl inline-flex gap-2 text-primary-400 font-bold p-6 pb-3 border-b border-primary-400 shadow-3xl">
-        <i className="bi bi-sticky"></i>
-        Make Offer
-      </h2>
-      <form className="p-6">
-        {Object.keys(defaultValues).map((name) => {
-          return /remark/gi.test(name) ? (
-            <FormTextAreaInput
-              key={name}
-              name={name as keyof TOfferForm}
-              register={register}
-              errors={errors}
-            />
-          ) : (
-            <FormInput
-              key={name}
-              name={name as keyof TOfferForm}
-              register={register}
-              errors={errors}
-            />
-          );
-        })}
-        {error instanceof FetchError && (
-          <ErrorMessage message={error.message} />
-        )}
-        <FormSubmitButton
-          className="flex items-center justify-center mt-4 ms-auto"
-          label="Confirm"
-          onClick={handleSubmit(onSubmit)}
-          disabled={isLoading}
-        />
-      </form>
-    </>
-  );
+	const isEdit = "offerId" in props;
+	const id = isEdit ? props.offerId : props.requestId;
+	const defaultValues = isEdit
+		? dtoToString(props.defaultValues)
+		: defaultEmptyValues;
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm<TOfferForm>({
+		resolver: zodResolver(offerSchema),
+		defaultValues,
+	});
+	const queryClient = useQueryClient();
+	const { setIsShow } = useControlModalContext();
+	const {
+		mutateAsync: mutateOffer,
+		error,
+		isLoading,
+	} = useMutation({
+		mutationFn: (data: TOfferForm) =>
+			isEdit ? putOfferAJAX(id, data) : postOfferAJAX(id, data),
+		onSuccess: async () => {
+			toast.success(`Offer has been made successfully!`);
+			isEdit
+				? await queryClient.invalidateQueries([
+						queryKey.OFFER,
+						{ offerId: props.offerId },
+				  ])
+				: await queryClient.invalidateQueries([queryKey.OFFER]);
+			setIsShow(false);
+		},
+	});
+	const onSubmit = async (form: TOfferForm) => {
+		await mutateOffer(form);
+	};
+	return (
+		<>
+			<h2 className="text-xl inline-flex gap-2 text-primary-400 font-bold p-6 pb-3 border-b border-primary-400 shadow-3xl">
+				<i className="bi bi-sticky"></i>
+				Make Offer
+			</h2>
+			<form className="p-6">
+				{Object.keys(defaultValues).map((name) => {
+					return /remark/gi.test(name) ? (
+						<FormTextAreaInput
+							key={name}
+							name={name as keyof TOfferForm}
+							register={register}
+							errors={errors}
+						/>
+					) : (
+						<FormInput
+							key={name}
+							name={name as keyof TOfferForm}
+							register={register}
+							errors={errors}
+						/>
+					);
+				})}
+				{error instanceof FetchError && (
+					<ErrorMessage message={error.message} />
+				)}
+				<FormSubmitButton
+					className="flex items-center justify-center mt-4 ms-auto"
+					label="Confirm"
+					onClick={handleSubmit(onSubmit)}
+					disabled={isLoading}
+				/>
+			</form>
+		</>
+	);
 };
 
 export default OfferForm;
