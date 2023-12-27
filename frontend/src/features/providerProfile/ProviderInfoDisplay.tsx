c407475140a3b77684ab7de1806f507817919b14
yt20chill <ytelainechiu@gmail.com>
2023-12-27 10:34:43 +0800

    added default biography for provider

--- frontend/src/features/providerProfile/ProviderInfoDisplay.tsx
+++ frontend/src/features/providerProfile/ProviderInfoDisplay.tsx
@@ -7,64 +7,78 @@ import Rating from "../../pages/user/requestDetails/components/Rating";
 import { GetProviderProfileDto } from "../../schemas/responseSchema";
 
 type ProviderInfoDisplayProps = {
-  dto: Omit<GetProviderProfileDto, "reviews">;
+	dto: Omit<GetProviderProfileDto, "reviews">;
 };
 
 const ProviderInfoDisplay = ({
-  dto: { providerName, starOfAttitude, starOfEfficiency, biography },
+	dto: {
+		providerName,
+		starOfAttitude,
+		starOfEfficiency,
+		biography,
+		numberOfReviews,
+	},
 }: ProviderInfoDisplayProps) => {
-  const { username, role } = useAuthStore(
-    useShallow((state) => ({ username: state.username, role: state.role }))
-  );
-  const { setIsShow } = useControlModalContext();
-  return (
-    <div className="flex flex-col p-6 bg-base-100 rounded-xl mb-6 sm:border border-slate-500/20 sm:mx-6">
-      <div className="flex gap-1 items-center">
-        <IconCircleFrame username={providerName} className="w-16 h-16" />
-        <div className="flex flex-col text-3xl ">
-          <span className="text-sm text-gray-400 indent-1">Provider</span>
-          {providerName}
-        </div>
-      </div>
-      <div className="flex items-center p-4 gap-6 w-max rounded-xl">
-        <img
-          src="/image/tags/tag_like_side.png"
-          className="w-20 h-20 bg-gray-400/20 rounded-lg p-2"
-        />
-        <div className="flex flex-col">
-          <h2 className="text-3xl">{starOfEfficiency.toFixed(1)}</h2>
-          <div className="text-xs text-base-content">Efficiency</div>
-          <div>
-            <Rating size="sm" score={starOfEfficiency} />
-          </div>
-        </div>
-        <div className="flex flex-col">
-          <h2 className="text-3xl">{starOfAttitude.toFixed(1)}</h2>
-          <div className="text-xs text-base-content">Attitude</div>
-          <div>
-            <Rating size="sm" score={starOfAttitude} />
-          </div>
-        </div>
-      </div>
-      <div className="flex gap-2 text-primary-400 ps-2 font-bold">
-        Bio
-        {role === AuthorizeLevels.PROVIDER && providerName === username && (
-          <span
-            className="text-gray-400 transition grid place-items-center rounded-full text-base-gray-400 hover:bg-gray-500/50 hover:-translate-y-[1px] hover:text-base-content/80 overflow-hidden w-6 h-6 cursor-pointer"
-            onClick={(e) => {
-              e.preventDefault();
-              setIsShow(true);
-            }}
-          >
-            <span className="material-symbols-rounded text-sm">edit</span>
-          </span>
-        )}
-      </div>
-      {biography && (
-        <p className="p-3 mb-6 bg-base-200/80 rounded-lg">{biography}</p>
-      )}
-    </div>
-  );
+	const { username, role } = useAuthStore(
+		useShallow((state) => ({ username: state.username, role: state.role }))
+	);
+	const { setIsShow } = useControlModalContext();
+	return (
+		<div className="flex flex-col p-6 bg-base-100 rounded-xl mb-6 sm:border border-slate-500/20 sm:mx-6">
+			<div className="flex gap-1 items-center">
+				<IconCircleFrame username={providerName} className="w-16 h-16" />
+				<div className="flex flex-col text-3xl ">
+					<span className="text-sm text-gray-400 indent-1">Provider</span>
+					{providerName}
+				</div>
+			</div>
+			<div className="flex items-center p-4 gap-6 w-max rounded-xl">
+				<img
+					src="/image/tags/tag_like_side.png"
+					className="w-20 h-20 bg-gray-400/20 rounded-lg p-2"
+				/>
+				{numberOfReviews > 0 ? (
+					<>
+						<div className="flex flex-col">
+							<h2 className="text-3xl">{starOfEfficiency.toFixed(1)}</h2>
+							<div className="text-xs text-base-content">Efficiency</div>
+							<div>
+								<Rating size="sm" score={starOfEfficiency} />
+							</div>
+						</div>
+						<div className="flex flex-col">
+							<h2 className="text-3xl">{starOfAttitude.toFixed(1)}</h2>
+							<div className="text-xs text-base-content">Attitude</div>
+							<div>
+								<Rating size="sm" score={starOfAttitude} />
+							</div>
+						</div>
+					</>
+				) : (
+					<div className="text-slate-400 capitalize">
+						No Reviews Available Yet
+					</div>
+				)}
+			</div>
+			<div className="flex gap-2 text-primary-400 ps-2 font-bold">
+				Bio
+				{role === AuthorizeLevels.PROVIDER && providerName === username && (
+					<span
+						className="text-gray-400 transition grid place-items-center rounded-full text-base-gray-400 hover:bg-gray-500/50 hover:-translate-y-[1px] hover:text-base-content/80 overflow-hidden w-6 h-6 cursor-pointer"
+						onClick={(e) => {
+							e.preventDefault();
+							setIsShow(true);
+						}}
+					>
+						<span className="material-symbols-rounded text-sm">edit</span>
+					</span>
+				)}
+			</div>
+			<p className="p-3 mb-6 bg-base-200/80 rounded-lg">
+				{biography ? biography : `Hello. I am ${providerName}`}
+			</p>
+		</div>
+	);
 };
 
 export default ProviderInfoDisplay;
--- frontend/src/features/providerProfile/ProviderProfileDisplay.tsx
+++ frontend/src/features/providerProfile/ProviderProfileDisplay.tsx
@@ -3,49 +3,49 @@ import { GetProviderProfileDto } from "../../schemas/responseSchema";
 import ProviderInfoDisplay from "./ProviderInfoDisplay";
 
 type ProviderProfileDisplayProps = {
-  dto: GetProviderProfileDto;
+	dto: GetProviderProfileDto;
 };
 
 const ProviderProfileDisplay = ({
-  dto: {
-    providerName,
-    starOfAttitude,
-    starOfEfficiency,
-    numberOfReviews,
-    biography,
-    reviews,
-  },
+	dto: {
+		providerName,
+		starOfAttitude,
+		starOfEfficiency,
+		numberOfReviews,
+		biography,
+		reviews,
+	},
 }: ProviderProfileDisplayProps) => {
-  return (
-    <>
-      {/* Info */}
-      <ProviderInfoDisplay
-        dto={{
-          providerName,
-          starOfAttitude,
-          starOfEfficiency,
-          numberOfReviews,
-          biography,
-        }}
-      />
-      {/* Reviews */}
-      <div className="flex flex-col bg-base-100 rounded-xl mb-6 sm:border border-slate-500/20 sm:mx-6">
-        <h2 className="font-bold p-4 pb-2 border-b border-slate-500/20 text-gray-500">
-          <i className="bi bi-chat-square-text-fill me-2"></i>Review (
-          {numberOfReviews})
-        </h2>
-        <div className="flex flex-col divide-y divide-slate-500/20">
-          {reviews && reviews.length > 0 ? (
-            reviews.map((review, index) => (
-              <ReviewCard key={`${review.item}-${index}`} dto={review} />
-            ))
-          ) : (
-            <p className="p-6">No reviews available yet</p>
-          )}
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			{/* Info */}
+			<ProviderInfoDisplay
+				dto={{
+					providerName,
+					starOfAttitude,
+					starOfEfficiency,
+					numberOfReviews,
+					biography,
+				}}
+			/>
+			{/* Reviews */}
+			<div className="flex flex-col bg-base-100 rounded-xl mb-6 sm:border border-slate-500/20 sm:mx-6">
+				<h2 className="font-bold p-4 pb-2 border-b border-slate-500/20 text-gray-500">
+					<i className="bi bi-chat-square-text-fill me-2"></i>Review (
+					{numberOfReviews})
+				</h2>
+				<div className="flex flex-col divide-y divide-slate-500/20">
+					{reviews && reviews.length > 0 ? (
+						reviews.map((review, index) => (
+							<ReviewCard key={`${review.item}-${index}`} dto={review} />
+						))
+					) : (
+						<p className="p-6 capitalize">No Reviews Available Yet</p>
+					)}
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default ProviderProfileDisplay;
