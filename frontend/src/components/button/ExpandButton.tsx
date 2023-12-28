d0c65f05df77823a092c465170ea9964c495dc8d
yt20chill <ytelainechiu@gmail.com>
2023-12-28 11:43:12 +0800

    added expand button according to requests page style

--- frontend/src/components/button/ExpandButton.tsx
+++ frontend/src/components/button/ExpandButton.tsx
@@ -6,13 +6,17 @@ type ExpandButtonProps = {
 const ExpandButton = ({ isExpanded, setIsExpanded }: ExpandButtonProps) => {
 	return (
 		<button
-			className="fixed left-0 top-1/2 z-10 hidden max-lg:block bg-primary-400 hover:bg-primary-500 text-white font-bold p-2 rounded-full shadow-md backdrop-blur-xl ring-offset-0 hover:shadow-lg hover:-translate-y-1 hover:ring-primary-300 hover:ring-2 hover:ring-offset-2 transition-all duration-500"
+			className="fixed left-0 top-1/2 z-20 hidden max-lg:block bg-primary-400 hover:bg-primary-500 text-white font-bold p-2 rounded-r-full  shadow-md backdrop-blur-xl ring-offset-0 hover:shadow-lg hover:-translate-y-1 hover:ring-primary-300 hover:ring-2 hover:ring-offset-2 transition-all duration-500 h-28"
 			onClick={(e) => {
 				e.preventDefault();
 				setIsExpanded(!isExpanded);
 			}}
 		>
-			{isExpanded ? "<" : ">"}
+			{isExpanded ? (
+				<i className="bi bi-caret-left-fill"></i>
+			) : (
+				<i className="bi bi-caret-right-fill"></i>
+			)}
 		</button>
 	);
 };
