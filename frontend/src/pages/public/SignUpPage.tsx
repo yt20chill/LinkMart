6d35b0c9b051eb21a22c9ebb01e020425bc2b146
yt20chill <ytelainechiu@gmail.com>
2023-12-28 12:32:06 +0800

    added tos to sign up page

--- frontend/src/pages/public/SignUpPage.tsx
+++ frontend/src/pages/public/SignUpPage.tsx
@@ -1,23 +1,49 @@
+import { SignPageAnimatedBG } from "@/features/animatedBackground/SignPageAnimatedBG";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import FormModal from "../../components/modal/FormModal";
+import TermsOfService from "../../features/TermsOfService";
 import SignUpForm from "../../features/forms/SignUpForm";
 import { useLoggedInRedirect } from "../../features/hooks/useLoggedInRedirect";
+import { ControlModalContext } from "../../services/context/ControlModalContext";
 import { RouteEnum, siteMap } from "../../services/routes.config";
-import { SignPageAnimatedBG } from "@/features/animatedBackground/SignPageAnimatedBG";
 
 const SignUpPage = () => {
-  useLoggedInRedirect();
-  return (
-    <>
-      <SignUpForm />
-      <p className="my-3 mx-auto font-roboto text-base tracking-wide [&_a]:hover:text-primary-500 [&_a]:hover:border-b-4  border-primary-500">
-        Already have an account?{" "}
-        <Link to={siteMap(RouteEnum.SignIn)} className="text-primary-300">
-          Sign In
-        </Link>
-      </p>
-      <SignPageAnimatedBG />
-    </>
-  );
+	useLoggedInRedirect();
+	const [showTOS, setShowTOS] = useState(false);
+	return (
+		<div className="flex flex-col justify-center">
+			<SignUpForm />
+			<div className="my-3 mx-auto font-roboto text-base tracking-wide [&_a]:hover:text-primary-500 [&_a]:hover:border-b-4  border-primary-500">
+				By registering, you accept our
+				<span
+					className="text-primary-300 hover:text-primary-500 cursor-pointer me-2 text-normal"
+					onClick={(e) => {
+						e.preventDefault();
+						setShowTOS(true);
+					}}
+				>
+					{" "}
+					Terms of Service
+				</span>
+			</div>
+			<div className="my-3 mx-auto font-roboto text-base tracking-wide [&_a]:hover:text-primary-500 [&_a]:hover:border-b-4  border-primary-500">
+				Already have an account?{" "}
+				<Link to={siteMap(RouteEnum.SignIn)} className="text-primary-300">
+					Sign In
+				</Link>
+			</div>
+
+			<ControlModalContext.Provider
+				value={{ isShow: showTOS, setIsShow: setShowTOS }}
+			>
+				<FormModal>
+					<TermsOfService />
+				</FormModal>
+			</ControlModalContext.Provider>
+			<SignPageAnimatedBG />
+		</div>
+	);
 };
 
 export default SignUpPage;
